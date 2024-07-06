4fd3bd272349d87d8c96a424418e53d2d7a0f824
Vishal7906 <viratrmr@gmail.com>
2024-07-06 19:53:36 +0530

    add user registeration validation

--- src/app.ts
+++ src/app.ts
@@ -7,14 +7,14 @@ import userRouter from "./user/userRouter";
 const app = express();
 
 // HTTP methods - GET , POST , PUT , PATCH , DELETE
-
+app.use(express.json()) // it is used to parse json data from the user
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
     const error = createHttpError(400,"Something Went Wrong")
     throw error;
     res.json({ message: "Welcome to E-Lib API" });
 });
-
 app.use('/api/users',userRouter);
+
 
 app.use(GlobalErrorHandler)
 export default app;
--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,7 +1,14 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Response,Request,NextFunction } from "express";
+import createHttpError from "http-errors";
 
 const createUser = async(req:Request,res:Response,next:NextFunction)=>{
+    const {name,email,password} = req.body;
+
+    if(!name || !email || !password){
+        const error = createHttpError(400,"All Field Required")
+        return next(error);
+    }
     res.json({message:"User Created Successfully"})
 }
 export {createUser}
